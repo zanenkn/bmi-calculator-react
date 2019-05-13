5ac14c16051a9cc8c951dd930c31cd1bbc4fe673
zanenkn <zane.neikena@gmail.com>
2019-05-13 13:26:12 +0200

    Scaffolds a React app, adds two input fields with states.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <>
+        <>
+          <label>Weight(kg)</label>
+          <input name="weight" value={this.state.weight} onChange={ (e) => this.setState({ weight: e.target.value}) } />
+        </>
+
+        <>
+          <label>Height(cm)</label>
+          <input name="height" value={this.state.height} onChange={ (e) => this.setState({ height: e.target.value}) } />
+        </>
+      </>
+    );
+  } 
+  
 }
 
 export default App;
