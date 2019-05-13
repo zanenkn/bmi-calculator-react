2913993867ef5ff6e1ce022930435c43c606c5d6
zanenkn <zane.neikena@gmail.com>
2019-05-13 13:31:04 +0200

    Adds a constructor

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      weight: '',
+      height: ''
+    }
+  }
+
   render() {
     return (
       <>
