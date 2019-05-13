685106b3c8ca71898c62fb378aea1acf9ed6a018
zanenkn <zane.neikena@gmail.com>
2019-05-13 15:36:01 +0200

    Creates the calculator logic code.

---
+++ src/Modules/BMICalculator.js
@@ -0,0 +1,37 @@
+export const bmiCalculation = (weight, height, method) => {
+  parseFloat(weight);
+  parseFloat(height);
+  let bmi;
+
+  weight = isNaN(weight) ? 0 : weight;
+  height = isNaN(height) ? 0 : height;
+
+  bmi= weight / (height / 100 * height / 100);
+  
+  let finalBMI = parseFloat(bmi.toFixed(2));
+  let BMIMessage = setBMIMessage(finalBMI)
+  if (isNaN(finalBMI) || !isFinite(finalBMI) || finalBMI === 0) {
+    return '';
+  } else {
+    return `You are ${BMIMessage} with a BMI of ${finalBMI}`;
+  }
+}
+
+const setBMIMessage = (finalBMI) => {
+  if (finalBMI < 18.5) {
+    return "underweight";
+  }
+
+  if (finalBMI > 18.5 && finalBMI < 25) {
+    return "normal";
+  }
+
+  if (finalBMI > 25 && finalBMI < 30) {
+    return "overweight";
+  }
+
+  if (finalBMI > 30) {
+    return "obese";
+  }
+}
+ 
